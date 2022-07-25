a03408412f95468f04fefed8ef0e67cce92cc68a
mjp9404 <99421557+mjp9404@users.noreply.github.com>
2022-07-25 17:16:09 -0600

    Update for Student Form and App driver

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import StudnetForm from './components/StudentForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <StudnetForm/>
+
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+
     </div>
   );
 }
--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -1,1 +1,45 @@
 //Reusable Studnet form
+import React from "react";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { FormGroup, FormControl, Button } from "react-bootstrap";
+
+const StudnetForm = (props) => {
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required("Requried"),
+        // ******************************************
+        // ????????? Needs to understand
+        // ******************************************
+        email: Yup.string().email("You have enter an invalid email address").required("Required"),
+        rollno: Yup.number().positive("Invalid roll number").integer("Invalid roll number").required("Required"),
+    });
+    console.log(props);
+    return (
+        <div>
+            <Formik {...props} validationSchema={validationSchema}>
+                <Form>
+                    {/* Display Student Name */}
+                    <FormGroup>
+                        <Field name="name" type="text" className="form-control" />
+                        <ErrorMessage name="name" className="d-block invalid-feedback" component="span" />
+                    </FormGroup>
+                    {/* Display Email */}
+                    <FormGroup>
+                        <Field name="email" type="text" className="d-block invalid-feedback" component="span" />
+                        <ErrorMessage name="email" className="d-block invalid-feedback" component="span" />
+                    </FormGroup>
+                    {/* Display Roll number */}
+                    <FormGroup>
+                        <Field name="rollno" type="number" className="d-block invalid-feedback" component="span" />
+                        <ErrorMessage name="rollno" className="d-block invalid-feedback" component="span" />
+                    </FormGroup>
+                    <Button variant="danger" size="lg" block="block" type="submit">
+                        {props.children}
+                    </Button>
+                </Form>
+            </Formik>
+        </div>
+    );
+};
+
+export default StudnetForm;
