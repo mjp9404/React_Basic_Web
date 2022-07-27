02c565fae39064a38c590cda8a3b612e1ee61b7b
mjp9404 <99421557+mjp9404@users.noreply.github.com>
2022-07-26 22:38:41 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,48 +4,47 @@ let cors = require('cors');
 let bodyParser = require('body-parser');
 let dbConfig = require('./database/db');
 
-
-const studentRoute =require('../backend/routes/student.route');
-
+// Express Route
+const studentRoute = require('../backend/routes/student.route')
 
 // Configure mongoDB Database
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useUnifiedTopology', true);
 
 // Connecting MongoDB Database
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.db).then(() => {
-  console.log('Database successfully connected!')
+console.log('Database successfully connected!')
 },
-  error => {
-    console.log('Could not connect to database : ' + error)
-  }
+error => {
+	console.log('Could not connect to database : ' + error)
+}
 )
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+extended: true
 }));
 app.use(cors());
 app.use('/students', studentRoute)
-  
-  
+
+
 // PORT
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
+console.log('Connected to port ' + port)
 })
-  
+
 // 404 Error
 app.use((req, res, next) => {
-  res.status(404).send('Error 404!')
+res.status(404).send('Error 404!')
 });
-  
+
 app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
+console.error(err.message);
+if (!err.statusCode) err.statusCode = 500;
+res.status(err.statusCode).send(err.message);
 });
